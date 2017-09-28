13577cd06eb3d4a532975dda0e8fb3cd15945276
yshen <yang.shen@anchora.me>
2017-09-28 13:22:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+this base is from https://github.com/dyc87112/SpringCloud-Learning/Chapter1-1-7
+init
---
+++ src/main/java/com/didispace/Application.java
@@ -0,0 +1,23 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+}
